0dac57747573d8b56aa8f92242ece67a40ed90d9
asuzukosi <keloasuzu@yahoo.com>
2023-10-21 19:50:43 +0100

    feat: add file structures

---
+++ src/core/__init__.py
@@ -0,0 +1,11 @@
+"""
+The core section of the modular transformer contains the key components of the architecture such as:
+1) Tokenizer
+2) Embedder
+3) Positional Encoder
+4) Transformer Block
+5) Attention Block (can be self, or cross)
+6) Point wise feedforward
+7) Encoder 
+8) Decoder
+"""
